6187a2734b31b576227ed6118527032712c540f4
Jon Borrelli <borrejj@gmail.com>
2016-12-13 22:30:43 -0500

    update interaction rscript

--- RScripts/interactions.R
+++ RScripts/interactions.R
@@ -2,16 +2,16 @@ library(MASS)
 m3l2 <- read.csv("Data/L2.csv", row.names = 1)
 
 r.m3l2 <- t(apply(m3l2, 2, function(x) x/sum(x)))
-r.m3l2A <- t(apply(m3l2, 2, function(x) x/sum(x)))[,which(apply(r.m3l2, 2, function(x) sum(x != 0)) >300)]
+r.m3l2A <- t(apply(m3l2[which(apply(r.m3l2, 2, function(x) sum(x != 0)) >300),], 2, function(x) x/sum(x)))
 
 v1 <- log(r.m3l2A[2:332,1]) - log(r.m3l2A[1:331,1])
 M <- apply(r.m3l2A, 2, function(x) x - median(x))
 
 mydat <- data.frame(M[1:331,], resp = v1)
 colnames(mydat) <- c(LETTERS[1:6], "resp")
-mydat$resp <- log(mydat$resp) - log(mydat$A)
+#mydat$resp <- log(mydat$resp) - log(mydat$A)
 
-stepAIC(lm(resp~A, data = mydat),list(lower = resp~A, upper= resp~A+B+C+D+E+F), direction = "forward", data = mydat)
+stepAIC(lm(resp~A, data = mydat[t1,]),list(lower = resp~A, upper= resp~A+B+C+D+E+F), direction = "forward", data = mydat[t1,])
 
 
 hlf <- length(1:332)/2
@@ -21,7 +21,8 @@ t2 <- t1-1
 fit <- lm(resp~A, data = mydat[t1,])
 error1 <- sum(predict(fit, newdata = mydat[-t1,]) - mydat[-t1,"resp"])
 
-fit1 <- lm(resp~A+F, data = mydat[t1,])
+fit1 <- lm(resp~A+E, data = mydat[t1,])
 error2 <- sum(predict(fit1, newdata = mydat[-t1,]) - mydat[-t1, "resp"])
 
 ((error1-error2)/error2)*100
+summary(fit)
